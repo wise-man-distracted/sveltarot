f1abd514828ed4bee251cea6c3ab9186364b0e31
Wellerson de Castro <magewelle@live.com>
2022-03-06 03:50:48 -0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sveltarot | App de tarô feito em Svelte/SvelteKit
+
+## Instalação
+1. Clone o repositório
+2. `npm i`
+3. `npm run dev`
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+	content: ['src/app.html', 'src/**/*.{svelte,css}'],
+	darkMode: 'class', // or 'media' or false
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
