3755678392be7af3e778d15d2e0b45f59cbacb55
Wellerson de Castro <magewelle@live.com>
2022-03-11 02:36:53 -0300

    handling api (has errors)

---
+++ src/routes/api/index.js
@@ -0,0 +1,5 @@
+export const get = () => {
+	return {
+		body: "string"
+	};
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,29 @@
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const res = await fetch("/api");
+		const data = await res.json();
+		console.log(data);
+
+		return {
+			props: {
+				data,
+			},
+		};
+	};
+</script>
+
 <script>
+	export let data;
 	import Counter from '$lib/Counter.svelte';
 </script>
+
+
 
 <div class="p-4 mx-auto text-center">
 	<h1
 		class="max-w-xs mx-auto my-16 text-6xl font-thin text-red-500 uppercase sm:max-w-none"
 	>
-		Hello world!
+	{JSON.stringify(data)}
 	</h1>
 
 	<Counter />
