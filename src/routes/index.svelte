dc361f08e7b0dc72d973cad9b77a57df6c2abb79
Wellerson de Castro <magewelle@live.com>
2022-03-12 01:27:18 -0300

    inicializando api e lógica de fetch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,13 +17,13 @@
 	import Counter from '$lib/Counter.svelte';
 </script>
 
-
+<pre>{JSON.stringify(data)}</pre>
 
 <div class="p-4 mx-auto text-center">
 	<h1
 		class="max-w-xs mx-auto my-16 text-6xl font-thin text-red-500 uppercase sm:max-w-none"
 	>
-	{JSON.stringify(data)}
+	Hello<!-- {JSON.stringify(data)} -->
 	</h1>
 
 	<Counter />
